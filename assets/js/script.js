701e6cf3b4f95af93b76f57f9c63c599342529af
MojosBeans100 <lucy.colley@btinternet.com>
2021-06-25 11:56:07 +0000

    Plan functions - create different 'screens' user will see, generate questions, check answers

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+
+// Create elements function
+
+// Intro screen function
+
+// Input name screen
+
+// Choose game options screen
+
+// Open game area screen
+ 
+// Generate question
+
+// Check answer
+
+// Show answers screen
+
+// End game screen
+
+// Pop-ups : 
+// Game difficult info pop-up
+// Choose an answer pop-up
+// You will lose progress pop-up 
