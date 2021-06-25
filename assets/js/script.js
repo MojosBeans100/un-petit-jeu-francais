e3072a1212c4a9bd8fffb0a7eb32979466dba350
MojosBeans100 <lucy.colley@btinternet.com>
2021-06-25 12:05:55 +0000

    Create introScreen function, add HTML elements for this screen and button function to link to next screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
-
+// Global variables
+// define the main div as a global variable as this will be used in lots of functions - saves having to "get" it each time
+let mainDiv = document.getElementById("main-div");
 
 // Create elements function
+function  createElement(elementType, id) {
+    let newElement = document.createElement(elementType);
+    newElement.id = id;
+    return newElement;
+}
 
 // Intro screen function
+function introScreen {
+
+    // Create intro screen elements and append to relevant divs
+    let introScreenDiv = createElement("div","intro-screen-div");
+    mainDiv.appendChild(introScreenDiv);
+
+    let introScreenTitle = createElement("h1","intro-screen-title");
+    introScreenDiv.appendChild(introScreenTitle);
+
+    let introScreenGameDescr = createElement("h2","intro-screen-game-des");
+    introScreenDiv.appendChild(introScreenGameDescr);
+
+    let introScreenBtn = createElement("button","intro-screen-btn");
+    introScreenDiv.appendChild(introScreenBtn);
+
+    // Create the text for the elements
+    introScreenTitle.innerHTML = "Welcome to Un Petit Jeu Francais!";
+    introScreenGameDescr.innerHTML="This website allows you to play a short, fun quiz to practice your level in French!";
+    introScreenBtn.innerHTML = "Play quiz";
+
+}
 
 // Input name screen
 
