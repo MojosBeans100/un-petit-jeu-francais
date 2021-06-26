63d3da9574b86c00d325b7c2fbc4956e0b475d83
MojosBeans100 <lucy.colley@btinternet.com>
2021-06-26 07:45:19 +0000

    Check radio buttons values to see what difficulty and length user chose for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,34 @@ let trueAnswers;
 let trueQuestions;
 
 // Create elements function
-function  createElement(elementType, id) {
+function createElement(elementType, id) {
     let newElement = document.createElement(elementType);
     newElement.id = id;
     return newElement;
 }
 
-document.addEventListener("DOMContentLoaded",introScreen);
+document.addEventListener("DOMContentLoaded", introScreen);
 
 // Intro screen function
 function introScreen() {
 
     // Create introScreen HTML elements and append to relevant divs
-    let introScreenDiv = createElement("div","intro-screen-div");
+    let introScreenDiv = createElement("div", "intro-screen-div");
     mainDiv.appendChild(introScreenDiv);
 
-    let introScreenTitle = createElement("h1","intro-screen-title");
+    let introScreenTitle = createElement("h1", "intro-screen-title");
     introScreenDiv.appendChild(introScreenTitle);
 
-    let introScreenGameDescr = createElement("h2","intro-screen-game-des");
+    let introScreenGameDescr = createElement("h2", "intro-screen-game-des");
     introScreenDiv.appendChild(introScreenGameDescr);
 
-    let introScreenBtn = createElement("button","intro-screen-btn");
+    let introScreenBtn = createElement("button", "intro-screen-btn");
     introScreenDiv.appendChild(introScreenBtn);
-    introScreenBtn.addEventListener("click",inputName);
+    introScreenBtn.addEventListener("click", inputName);
 
     // Create the text for the elements
     introScreenTitle.innerHTML = "Welcome to Un Petit Jeu Francais!";
-    introScreenGameDescr.innerHTML="This website allows you to play a short, fun quiz to practice your level in French!";
+    introScreenGameDescr.innerHTML = "This website allows you to play a short, fun quiz to practice your level in French!";
     introScreenBtn.innerHTML = "Play quiz";
 
 }
@@ -49,18 +49,18 @@ function inputName() {
     introScreenDiv.remove();
 
     // Create inputName HTML elements and append to relevant divs
-    let inputNameDiv = createElement("diV","input-name-div");
+    let inputNameDiv = createElement("diV", "input-name-div");
     mainDiv.appendChild(inputNameDiv);
 
-    let inputNameText = createElement("h1","input-name-text");
+    let inputNameText = createElement("h1", "input-name-text");
     inputNameDiv.appendChild(inputNameText);
 
-    let inputNameInput = createElement("input","input-name-input");
+    let inputNameInput = createElement("input", "input-name-input");
     inputNameDiv.appendChild(inputNameInput);
 
-    let inputNameBtn = createElement("button","input-name-btn");
+    let inputNameBtn = createElement("button", "input-name-btn");
     inputNameDiv.appendChild(inputNameBtn);
-    inputNameBtn.addEventListener("click",gameOptions);
+    inputNameBtn.addEventListener("click", gameOptions);
 
     // Create the text for the elements
     inputNameText.innerHTML = "First, let's start with your name";
@@ -78,29 +78,29 @@ function gameOptions() {
     document.getElementById("input-name-div").remove();
 
     // Create gameOptions HTML elements and append to relevant divs
-    let gameOptionsDiv = createElement("div","game-options-div");
+    let gameOptionsDiv = createElement("div", "game-options-div");
     mainDiv.appendChild(gameOptionsDiv);
 
-    let gameOptionsWelcomeName = createElement("h1","game-options-welcome-name");
+    let gameOptionsWelcomeName = createElement("h1", "game-options-welcome-name");
     gameOptionsDiv.appendChild(gameOptionsWelcomeName);
 
-    let gameOptionsText = createElement("h2","game-options-text");
+    let gameOptionsText = createElement("h2", "game-options-text");
     gameOptionsDiv.appendChild(gameOptionsText);
 
-    let gameOptionsForm = createElement("form","game-difficulty-form");
+    let gameOptionsForm = createElement("form", "game-difficulty-form");
     gameOptionsDiv.appendChild(gameOptionsForm);
 
-    let gameOptionsDifficultyText = createElement("h3","game-options-difficulty-text");
+    let gameOptionsDifficultyText = createElement("h3", "game-options-difficulty-text");
     gameOptionsForm.appendChild(gameOptionsDifficultyText);
 
     // Create difficulty radio options
-    let gameDifficultyOptions = ["Easy","Medium","Hard"];
+    let gameDifficultyOptions = ["Easy", "Medium", "Hard"];
     let gameDifficultyOptionsID = [];
 
     for (let i = 0; i < gameDifficultyOptions.length; i++) {
 
         // Create radio buttons
-        var diffRadio = createElement("input",`${gameDifficultyOptions[i]}`);
+        var diffRadio = createElement("input", `${gameDifficultyOptions[i]}`);
         diffRadio.type = "radio";
         diffRadio.name = "diffRadioName";
         diffRadio.checked = false;
@@ -117,16 +117,16 @@ function gameOptions() {
         gameDifficultyOptionsID.push(diffRadio.id);
     }
 
-    let gameOptionsLengthText = createElement("h3","game-options-length-text");
+    let gameOptionsLengthText = createElement("h3", "game-options-length-text");
     gameOptionsForm.appendChild(gameOptionsLengthText);
 
     // Create game length options
-    let gameLengthOptions = ["10","20","30","40"];
+    let gameLengthOptions = ["10", "20", "30", "40"];
 
     for (let i = 0; i < gameLengthOptions.length; i++) {
 
         // Create radio buttons
-        var lengthRadio = createElement("input",`gameLength${gameLengthOptions[i]}`);
+        var lengthRadio = createElement("input", `gameLength${gameLengthOptions[i]}`);
         lengthRadio.type = "radio";
         lengthRadio.name = "lengthRadioName";
         lengthRadio.checked = false;
@@ -135,7 +135,7 @@ function gameOptions() {
         // Create radio button labels
         var lengthLabel = document.createElement("label");
         lengthLabel.innerHTML = gameLengthOptions[i];
-        
+
         // Append radios and labels to form
         gameOptionsForm.appendChild(lengthRadio);
         gameOptionsForm.appendChild(lengthLabel);
@@ -145,12 +145,12 @@ function gameOptions() {
     Easy.checked = true;
     gameLength10.checked = true;
 
-    let gameOptionsBtn = createElement("button","game-options-btn");
-    gameOptionsBtn.addEventListener("click",function() {
-        alert("HELLO");
+    let gameOptionsBtn = createElement("button", "game-options-btn");
+    gameOptionsBtn.addEventListener("click", function () {
+        createGameArea();
     });
     gameOptionsDiv.appendChild(gameOptionsBtn);
-    
+
     // Create the text for the elements
     gameOptionsWelcomeName.innerHTML = (`Welcome ${userName}!`);
     gameOptionsText.innerHTML = "Now choose some game options:";
@@ -163,20 +163,37 @@ function gameOptions() {
 // Open game area screen
 function createGameArea() {
 
+    // Get user's chosen difficulty
+    for (i = 0; i < 3; i++) {
+        if (document.getElementsByTagName("input")[i].checked) {
+            gameDifficulty = document.getElementsByTagName("label")[i].innerHTML;
+            console.log(gameDifficulty);
+        }
+    }
+
+    // Get user's chosen game length
+    for (i = 3; i < 7; i++) {
+        if (document.getElementsByTagName("input")[i].checked) {
+            gameLength = document.getElementsByTagName("label")[i].innerHTML;
+            console.log(gameLength);
+        }
+    }
+
+
     // Create the main game area div
-    let gameAreaDiv = createElement("div","game-area-div");
+    let gameAreaDiv = createElement("div", "game-area-div");
     mainDiv.appendChild(gameAreaDiv);
 
     // Create the div with question, multiple choice and buttons
-    let gameAreaLeft = createElement("div","game-area-left");
-    let gameAreaLeft1 = createElement("div","game-area-left-1");
-    let gameAreaLeft2 = createElement("div","game-area-left-2");
-    let gameAreaLeft3 = createElement("div","game-area-left-3");
+    let gameAreaLeft = createElement("div", "game-area-left");
+    let gameAreaLeft1 = createElement("div", "game-area-left-1");
+    let gameAreaLeft2 = createElement("div", "game-area-left-2");
+    let gameAreaLeft3 = createElement("div", "game-area-left-3");
 
     // Create the div with progress tally and scores (------and timer)
-    let gameAreaRight = createElement("div","game-area-right");
-    let gameAreaRight1 = createElement("div","game-area-right-1");
-    let gameAreaRight2 = createElement("div","game-area-right-2");
+    let gameAreaRight = createElement("div", "game-area-right");
+    let gameAreaRight1 = createElement("div", "game-area-right-1");
+    let gameAreaRight2 = createElement("div", "game-area-right-2");
 
     gameAreaDiv.appendChild(gameAreaLeft);
     gameAreaDiv.appendChild(gameAreaRight);
@@ -188,66 +205,71 @@ function createGameArea() {
     gameAreaRight.appendChild(gameAreaRight1);
     gameAreaRight.appendChild(gameAreaRight2);
 
-    let homeButton = createElement("button","game-home-btn");
+    // Create buttons
+    let homeButton = createElement("button", "game-home-btn");
     homeButton.innerHTML = "Return to Home";
-    homeButton.addEventListener("click",function() {
+    homeButton.addEventListener("click", function () {
         alert("Return to home?");
     });
 
-    let skipButton = createElement("button","skip-question-btn");
+    let skipButton = createElement("button", "skip-question-btn");
     skipButton.innerHTML = "Skip question";
     // skipButton.addEventListener("click", skipQuestion);
 
-    let nextQuestionButton = createElement("button","next-question-btn");
+    let nextQuestionButton = createElement("button", "next-question-btn");
     nextQuestionButton.innerHTML = "Next question";
     // nextQuestionButton.addEventListener("click", checkAnswer);
 
-} 
-
-// Generate question
-function generateQuestion() {
-
-    // get user's answers
-
-    let mcRandomNums = [];
-    let mcQuestions = [];
-    let mcAnswers = [];
-
-    for (i = 0; i < numOfQuestions; i ++) {
-
-        // Create random numbers between 1 and max length of language array
-        mcRandomNums[i] = Math.floor(Math.random() * options[questionLanguage][gameDifficulty].length);
-
-        // Index words from language arrays using these random numbers
-        mcQuestions[i] = options[questionLanguage][gameDifficulty][mcRandomNums[i]];
-        mcAnswers[i] = options[answerLanguage][gameDifficulty][mcRandomNums[i]];
-
-        // Remove the chosen words in each language to prevent them being repeated
-        options[questionLanguage][gameDifficulty].splice(mcRandomNums[i]), 1);
-        options[answerLanguage][gameDifficulty].splice(mcRandomNums[i]), 1);
-    }
-
-    // Pick a value between 1 and 4 (5 for med, 6 for hard) to be the "chosen" question and answer word
-    let chosenRandomNum = Math.floor(Math.random()* (numOfQuestions - 1)) + 1;
-
-    // Index the chosen question and answer from multiple choice
-    mcAnswer = mcAnswers[chosenRandomNum];
-    mcAnswer.id = "mc-answer";
-    trueAnswers.push(mcAnswer);
-
-    mcQuestion = mcQuestions[chosenRandomNum];
-    mcQuestion.id = "mc-question";
-    trueQuestions.push(mcQuestion);
-
-    // Create the question
-    let questionText = createElement("h1","question-text");
-    questionText.innerHTML = (`What is ${mcAnswer} in ${questionLanguage}?`);
-    // append to HTML
-
-    // Create the multiple choice radio buttons
-
+    gameAreaLeft3.appendChild(homeButton);
+    gameAreaLeft3.appendChild(skipButton);
+    gameAreaLeft3.appendChild(nextQuestionButton);
 
 }
+
+// // Generate question
+// function generateQuestion() {
+
+//     // get user's answers
+
+//     let mcRandomNums = [];
+//     let mcQuestions = [];
+//     let mcAnswers = [];
+
+//     for (i = 0; i < numOfQuestions; i ++) {
+
+//         // Create random numbers between 1 and max length of language array
+//         mcRandomNums[i] = Math.floor(Math.random() * options[questionLanguage][gameDifficulty].length);
+
+//         // Index words from language arrays using these random numbers
+//         mcQuestions[i] = options[questionLanguage][gameDifficulty][mcRandomNums[i]];
+//         mcAnswers[i] = options[answerLanguage][gameDifficulty][mcRandomNums[i]];
+
+//         // Remove the chosen words in each language to prevent them being repeated
+//         options[questionLanguage][gameDifficulty].splice(mcRandomNums[i]), 1);
+//         options[answerLanguage][gameDifficulty].splice(mcRandomNums[i]), 1);
+//     }
+
+//     // Pick a value between 1 and 4 (5 for med, 6 for hard) to be the "chosen" question and answer word
+//     let chosenRandomNum = Math.floor(Math.random()* (numOfQuestions - 1)) + 1;
+
+//     // Index the chosen question and answer from multiple choice
+//     mcAnswer = mcAnswers[chosenRandomNum];
+//     mcAnswer.id = "mc-answer";
+//     trueAnswers.push(mcAnswer);
+
+//     mcQuestion = mcQuestions[chosenRandomNum];
+//     mcQuestion.id = "mc-question";
+//     trueQuestions.push(mcQuestion);
+
+//     // Create the question
+//     let questionText = createElement("h1","question-text");
+//     questionText.innerHTML = (`What is ${mcAnswer} in ${questionLanguage}?`);
+//     // append to HTML
+
+//     // Create the multiple choice radio buttons
+
+
+// }
 
 
 // Check answer
